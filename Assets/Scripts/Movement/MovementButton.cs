fcaff3df216045f301e8946f62e9b2ff5ab2e2ab
Tevadu52 <tevafoug@gmail.com>
2025-03-02 21:31:36 +0100

    [MODIFY] Better UI InGame

--- Assets/Scripts/Movement/MovementButton.cs
+++ Assets/Scripts/Movement/MovementButton.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -21,13 +22,22 @@ public class MovementButton : MonoBehaviour
 
     void OnMouseDown()
     {
-        if(isPlaying(GameManager.Instance.AnimPlayer, "anim_idle") && Time.timeScale != 0)
+        if(!IsPointerOverUIObject() && isPlaying(GameManager.Instance.AnimPlayer, "anim_idle") && Time.timeScale != 0)
         {
             Vector3 newPos = transform.position;
             newPos.z = 0;
             _mouvementPlayer.AddPos(newPos);
             _mouvementPlayer.StartMoving();
         }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 
     bool isPlaying(Animator anim, string stateName)
